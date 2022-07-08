799b7435014a1dbe3d96f9b3835904e99f5666e9
Jakob <jakob.hjortshoj@noaignite.com>
2022-07-08 12:19:26 +0200

    exchange rate added for dynamic code

---
+++ LOTR Converter/LOTR Converter/ExchangeInfoView.swift
@@ -0,0 +1,62 @@
+//
+//  ExchangeInfo.swift
+//  LOTR Converter
+//
+//  Created by Jakob Hjortshøj on 08/07/2022.
+//
+
+import SwiftUI
+
+struct ExchangeInfoView: View {
+    var body: some View {
+            ZStack {
+                Image("parchment")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .background(.brown)
+                
+                VStack( spacing: 13){
+                    Text("Exchange Rates")
+                        .font(.largeTitle)
+                    
+                    Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world,except one. We used to take Brandu Bucks but after finding out that is was a person instead of a piece of paper, we realized it had no value to s. Below is a simple guide to our currency exchange rates:")
+                        .padding()
+                        .lineSpacing(3)
+                        .font(.title2)
+                        .foregroundColor(.black.opacity(0.7))
+                    
+                    HStack{
+                        ExchangeRate(leftImage: "goldpiece", text: "1 Gold Piece = 4 Gold Pieces", rightImage: "goldpenny")
+                    }
+                    HStack{
+                        ExchangeRate(leftImage: "goldpenny", text: "1 Gold Penny = 4 Silver Pieces", rightImage: "silverpiece")
+                    }
+                    HStack{
+                        ExchangeRate(leftImage: "silverpiece", text: "1 Silver Piece = 4 Silver Pennies", rightImage: "silverpenny")
+                    }
+                    HStack{
+                        ExchangeRate(leftImage: "silverpenny", text: "1 Silver Penny = 100 Copper Pennie", rightImage: "copperpenny")
+                    }
+                    Button {
+                        
+                    } label: {
+                        Text("Done")
+                    }
+                    .padding(8)
+                    .font(.system(size: 27, weight: .light))
+                    .foregroundColor(Color(UIColor.systemGray5))
+                    .background(.brown.opacity(0.9))
+                    .cornerRadius(10)
+
+                        
+            }
+        }
+        
+    }
+}
+
+struct ExchangeInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        ExchangeInfoView()
+    }
+}
--- LOTR Converter/LOTR Converter/ExchangeRate.swift
+++ LOTR Converter/LOTR Converter/ExchangeRate.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ExchangeRate: View {
+    
+    @State var leftImage: String
+    @State var text: String
+    @State var rightImage: String
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            
+            Text(text)
+                .foregroundColor(.black.opacity(0.8))
+            
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+        }
     }
 }
 
 struct ExchangeRate_Previews: PreviewProvider {
     static var previews: some View {
-        ExchangeRate()
+        ExchangeRate(leftImage: "goldpenny", text: "1 Gold Penny = 4 Silver Pieces", rightImage: "silverpiece")
+            .previewLayout(.sizeThatFits)
     }
 }
