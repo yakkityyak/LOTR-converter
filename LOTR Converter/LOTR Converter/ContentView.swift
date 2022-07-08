cd809c87e65b38712f924229abef8457a9d702e8
Jakob <jakob.hjortshoj@noaignite.com>
2022-07-08 10:23:44 +0200

    Images and appcioins added + image, text and button added

--- LOTR Converter/LOTR Converter/ContentView.swift
+++ LOTR Converter/LOTR Converter/ContentView.swift
@@ -9,13 +9,78 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image("prancingpony")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+
+                
+                HStack{
+                    
+                    VStack{
+                        
+                        HStack {
+                            Image("silverpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                        }
+                        
+                        Text("Text Field")
+                    }
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    
+                    VStack{
+                        
+                        HStack {
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            
+                            Image("goldpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            }
+                        
+                        Text("Text field")
+                    }
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                    }
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding()
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
