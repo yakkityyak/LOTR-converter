e3810c77a598262f78f450e0f9e44d61e8b390e6
Jakob <jakob.hjortshoj@noaignite.com>
2022-07-08 09:49:05 +0200

    Project setup done

---
+++ LOTR Converter/LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Jakob Hjortshøj on 08/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
